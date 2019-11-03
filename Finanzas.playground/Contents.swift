4b38218b1c609e698d509f316c827d3b607a8378
Daniel Caldera <bdcaldera@gmail.com>
2019-11-02 20:02:44 -0600

    Second Push

--- Finanzas.playground/Contents.swift
+++ Finanzas.playground/Contents.swift
@@ -1,118 +1,100 @@
 import Foundation
 
-var accountTotal: Float = 1_000_000.0
-
-let name = "Andres"
-let lastName = "Silva"
-
-let fullName = "\(name) \(lastName)"
-
-print(fullName.capitalized)
-
-accountTotal += 100_000
-
-print(accountTotal)
-
-var isActive = !fullName.isEmpty
-
-print(isActive)
-
-var transactions: [Float] = [20, 10, 100.0]
-
-print(transactions)
-
-transactions.count
-transactions.isEmpty
-
-transactions.append(40)
-
-print(transactions)
-
-transactions.first
-transactions.last
-
-transactions.removeLast()
-
-print(transactions)
-
-transactions.min()
-transactions.max()
-
-var dailyTransaction: [[Float]] = [
-    [20, 10, 100.0],
-    [],
-    [1000],
-    [],
-    [10]
-]
-
-dailyTransaction.first
-dailyTransaction[4].isEmpty
-
-var transactionsDict: [String: [Float]] = [
-    "1nov": [20, 10, 100.0],
-    "2nov": [],
-    "3nov": [1000],
-    "4nov": [],
-    "5nov": [10]
-]
-
-var transactionsDict2: [String: [Float]] = [
-    "2nov": [1000],
-    "3nov": [1000],
-    "4nov": [1000],
-]
-
-func totalAccount(forTransactions transactions: [String: [Float]]) -> (Float, Int) {
-    var total: Float = 0
-    for key in transactions.keys {
-        let array = transactions[key]!
-        total += array.reduce(0.0, +)
+class Transaction {
+    var value: Float
+    var name: String
+    
+    init(value: Float, name: String) {
+        self.value = value
+        self.name = name
     }
-    return (total, transactions.count)
 }
 
-let total = totalAccount(forTransactions: transactionsDict)
-let total2 = totalAccount(forTransactions: transactionsDict2)
-
-print(total.0, total.1)
-print(total2)
-
-let name2 = (name: "Daniel", lastName: "Caldera")
-
-print(name2.name)
-print(name2.lastName)
-
-var a = 1
-var b = 2
-var c = 0
-
-//c = a
-//a = b
-//b = c
-
-(a, b) = (b, a)
-
-@discardableResult
-func addTransaction(transactionValue value: Float?) -> (Float, Bool) {
-    guard let value = value else {
-        return (accountTotal, false)
-    }
-    
-    value
-    
-    if (accountTotal - value) < 0 {
-        return (accountTotal, false)
-    }
-    accountTotal -= value
-    transactions.append(value)
-    return (accountTotal, true)
+class Debit : Transaction {
     
 }
 
-let value1 = addTransaction(transactionValue: nil)
+class Gain : Transaction {
+    
+}
 
-let value2 = addTransaction(transactionValue: 30)
+class Acccount {
+    var amount: Float = 0 {
+        willSet {
+            print("Vamos a modificar esta variable", newValue)
+        }
+        didSet {
+            print("Tenemos nuevo valor", amount)
+        }
+    }
+    var name: String = ""
+    var transactions: [Transaction] = []
+    
+    init(amount: Float, name: String) {
+        self.amount = amount
+        self.name = name
+    }
+    
+    @discardableResult
+    func addTransaction(transaction: Transaction) -> Float {
+        if transaction is Gain {
+            amount += transaction.value
+        }
+        if transaction is Debit {
+            if (amount - transaction.value) < 0 {
+                return 0
+            }
+            amount -= transaction.value
+        }
+        transactions.append(transaction)
+        return amount
+    }
+    func debits() -> [Transaction] {
+        return transactions.filter({ $0 is Debit })
+    }
+    func gains() -> [Transaction] {
+        return transactions.filter({ $0 is Gain })
+    }
+}
 
-print(value1)
-print(value2)
+class Person {
+    var name: String = ""
+    var lastName: String = ""
+    var account: Acccount?
+    
+    var fullName: String {
+        get {
+            return "\(name) \(lastName)"
+        }
+        set {
+            name = String((newValue.split(separator: " ").first) ?? "No tienes nombre")
+            lastName = "\(newValue.split(separator: " ").last ?? "No tienes apellido")"
+        }
+    }
+    
+    init(name: String, lastName: String) {
+        self.name = name
+        self.lastName = lastName
+    }
+}
+
+var me = Person(name: "Daniel", lastName: "Caldera")
+
+var account = Acccount(amount: 100_000, name: "X bank")
+
+me.account = account
+
+print(me.account!)
+
+me.account?.addTransaction(
+    transaction: Debit(value: 20, name: "Chicle"))
+me.account?.addTransaction(
+    transaction: Debit(value: 100, name: "Celular nuevo"))
+me.account?.addTransaction(
+    transaction: Debit(value: 500, name: "Consola PS5"))
+//account.addTransaction(value: 20)
+
+me.account?.addTransaction(
+    transaction: Gain(value: 1000, name: "Salario"))
+
+print(me.account!.amount)
