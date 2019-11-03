539329aea740514ece93ad696536485388fa357c
Daniel Caldera <bdcaldera@gmail.com>
2019-11-03 11:14:52 -0600

    Class4Enums

--- Finanzas.playground/Contents.swift
+++ Finanzas.playground/Contents.swift
@@ -1,120 +1,153 @@
 import Foundation
 
-class Person{
-    var name:String
-    var lastName:String
+enum DebitCategories {
+    case health
+    case food, rent, tax, transportation
+    case entertainment
+}
+
+class Transaction {
+    var value: Float
+    var name: String
     
-    init(name:String, lastName:String) {
+    init(value: Float, name: String) {
+        self.value = value
+        self.name = name
+    }
+}
+
+class Debit: Transaction {
+    var category: DebitCategories
+
+    init(value: Float, name: String, category: DebitCategories) {
+        self.category = category
+        super.init(value: value, name: name)
+    }
+}
+
+class Gain: Transaction {
+    
+}
+
+class Acccount {
+    var amount: Float = 0 {
+        willSet {
+            print("Vamos a cambiar el valor", amount, newValue)
+        }
+        didSet {
+            print("Tenemos nuevo valor", amount)
+        }
+    }
+    
+    var name: String = ""
+    var transactions: [Transaction] = []
+    
+    init(amount: Float, name: String) {
+        self.amount = amount
+        self.name = name
+    }
+    
+    @discardableResult
+    func addTransaction(transaction: Transaction) -> Float {
+        if transaction is Gain {
+            amount += transaction.value
+        }
+        
+        if transaction is Debit {
+            if (amount - transaction.value) < 0 {
+                return 0
+            }
+            
+            amount -= transaction.value
+        }
+        
+        transactions.append(transaction)
+        
+        return amount
+    }
+    
+    func debits() -> [Transaction] {
+        return transactions.filter({ $0 is Debit })
+    }
+    
+    func gains() -> [Transaction] {
+        return transactions.filter({ $0 is Gain })
+    }
+    
+    func transactionsFor(category: DebitCategories) -> [Transaction] {
+        return transactions.filter({ (transaction) -> Bool in
+            guard let transaction = transaction as? Debit else {
+                return false
+            }
+            
+            return transaction.category == category
+        })
+    }
+}
+
+class Person {
+    var name: String = ""
+    var lastName: String = ""
+    var account: Acccount?
+    
+    var fullName: String {
+        get {
+            return "\(name) \(lastName)"
+        }
+        set {
+            name = String(newValue.split(separator: " ").first ?? "")
+            lastName = "\(newValue.split(separator: " ").last ?? "")"
+        }
+    }
+    
+    init(name: String, lastName: String) {
         self.name = name
         self.lastName = lastName
     }
-    
-    func getFullName() -> String {
-        return"\(name)\(lastName)"
-    }
 }
 
-class Account{
-    var person:Person
-    var transactions:[Transaction]
-    
-    init(person:Person, transactions:[Transaction] = []) {
-        self.person = person
-        self.transactions = transactions;
-    }
-    
-    func getPerson() -> Person {
-        return person
-    }
-    
-    func getMount() -> Float {
-        if (transactions.count == 0) {
-            return 0
-        }
-        
-        let addTransactions:[Float] = transactions.filter( {$0.type == "add" }).map({ return $0.value })
-        let sumAddTransactions:Float = addTransactions.map({$0}).reduce(0, +);
-        
-        let debitTransactions:[Float] = transactions.filter( {$0.type == "debit" }).map({ return $0.value })
-        let sumDebitTransactions:Float = debitTransactions.map({$0}).reduce(0, +);
-        
-        return sumAddTransactions - sumDebitTransactions;
-    }
-    
-    func addTransaction(transaction:Transaction) {
-        let currentAccountMount = getMount();
-        
-        if(transaction.type == "debit" && (currentAccountMount - transaction.value) < 0){
-            print("esta transaction no puede ser mayor al monto a \(currentAccountMount) ")
-        } else {
-            transactions.append(transaction)
-        }
-    }
-    
-    func getTransactions() -> [Transaction] {
-        return transactions;
-    }
+var me = Person(name: "Andres", lastName: "Silva")
+
+let account = Acccount(amount: 100_000, name: "X bank")
+
+me.account = account
+
+print(me.account!)
+
+me.account?.addTransaction(
+    transaction: Debit(
+        value: 20,
+        name: "Cafe con amigos",
+        category: DebitCategories.food
+    )
+)
+
+me.account?.addTransaction(
+    transaction: Debit(
+        value: 100,
+        name: "Juego PS4",
+        category: .entertainment
+    )
+)
+
+me.account?.addTransaction(
+    transaction: Debit(
+        value: 500,
+        name: "PS4",
+        category: .entertainment
+    )
+)
+
+me.account?.addTransaction(
+    transaction: Gain(value: 1000, name: "Salario")
+)
+
+print(me.account!.amount)
+
+let transactions = me.account?.transactionsFor(category: .entertainment) as? [Debit]
+for transaction in transactions ?? [] {
+    print(transaction.name, transaction.value, transaction.category)
 }
 
-class Transaction{
-    var account:Account
-    var value:Float;
-    var description:String
-    var type:String = "add"
-    
-    init(account:Account, value:Float, description:String) {
-        self.account = account
-        self.value = value
-        self.description = description
-    }
-    
-    func setAccount(account:Account) {
-        self.account = account;
-    }
-    
-    func setValue(value:Float) {
-        self.value = value;
-    }
-    
-    func setDescription(description:String) {
-        self.description = description;
-    }
-    
-    func setType(type:String) {
-        self.type = type;
-    }
-    
-    func getType() -> String {
-        return type
-    }
-    
-    func getValue() -> Float {
-        return value
-    }
-}
 
-class Debit: Transaction{
-    override init(account: Account, value: Float, description: String) {
-        super.init(account: account, value: value, description: description)
-        setType(type: "debit")
-    }
-}
 
-class Add: Transaction{
-    override init(account: Account, value: Float, description: String) {
-        super.init(account: account, value: value, description: description)
-        setType(type: "add")
-    }
-}
-
-let person:Person = Person(name:"Daniel", lastName:"Caldera")
-
-let account:Account = Account(person: person);
-
-account.addTransaction(transaction: Add(account: account, value: 2000, description: "Pago por freelance"))
-
-account.addTransaction(transaction: Debit(account: account, value: 450, description: "Mercado para el mes"))
-
-account.addTransaction(transaction: Debit(account: account, value: 500, description: "Pago alquiler mes junio"))
-
-print(account.getPerson().getFullName(), "Tu saldo es \(account.getMount())")
