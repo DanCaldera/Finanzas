c8746ef6d1982768d3478bbc28254c0f035be831
Daniel Caldera <bdcaldera@gmail.com>
2019-11-03 23:29:52 -0600

    Fx01

--- Finanzas.playground/Contents.swift
+++ Finanzas.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 var me = Person(name: "Daniel", lastName: "Caldera")
 
-let account = Acccount(amount: 100_000, name: "X bank")
+let account = Acccount(amount: 10_000, name: "X bank")
 
 me.account = account
 
@@ -76,29 +76,25 @@ DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
 
 print(me.account!.amount)
 
-DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-    let transactions = me.account?.transactionsFor(category: .entertainment) as? [Debit]
-    for transaction in transactions ?? [] {
-        print(
-            "Hello",
-            transaction.name,
-            transaction.value,
-            transaction.category.rawValue,
-            transaction.date
-        )
-    }
-    
-    for gain in me.account?.gains ?? [] {
-        print(
-            "Hello gain",
-            gain.name,
-            gain.isValid,
-            gain.value,
-            gain.date
-        )
-    }
-}
-
-
 print(me.account?.amount ?? 0)
 
+print(me.account?.lifeTimeValue())
+
+enum HttpMethods: String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+print(HttpMethods.delete.rawValue)
+
+func sum<N: Numeric>(a: N, b: N) -> N {
+    return a + b
+}
+
+print(sum(a: 10, b: 12))
+
+let array = [2, 21, 30, 5, 20, 11, 14, 10]
+for element in array where element > 10 {
+    print(element)
+}
